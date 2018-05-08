ba52d6e83182097abd037855e2648a50549f82d0
jiangxiangxuan <jiangxiangxuan@126.com>
2018-05-09 00:18:48 +0800

    gameserver

--- server/Engine/Kernal/KernalServerBase.cpp
+++ server/Engine/Kernal/KernalServerBase.cpp
@@ -208,9 +208,9 @@ void KernalServerBase::epollWroker()
 }
 
 #if defined(KERNAL_USE_COMMUNICATION_PIPE)
-	void worker(KernalCommunicationPipe *pComPipe);
+void KernalServerBase::worker(KernalCommunicationPipe *pComPipe);
 #else
-	void worker();	
+void KernalServerBase::worker();	
 #endif
 {
 	onWorkerPre();
