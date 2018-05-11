6ccbcef8086a62956517d6f74788df9a68206974
jiangxiangxuan <jiangxiangxuan@126.com>
2018-05-11 11:39:25 +0800

    gameserver

--- server/Engine/Kernal/KernalGuid.cpp
+++ server/Engine/Kernal/KernalGuid.cpp
@@ -21,5 +21,6 @@ bool guidInit()
 unsigned long int getGuid()
 {
 	unsigned long int curTime = getCurrentTime();
+	printf("getGuid g_guid=%ld  time=%ld  guid = %ld\r\n", g_guid, curTime, g_guid ^ curTime);
 	return g_guid ^ curTime;
 }
