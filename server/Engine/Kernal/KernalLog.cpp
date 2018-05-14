9f87ad085f4a2a029a5baaa2cf084d0e330b0cef
jiangxiangxuan <jiangxiangxuan@126.com>
2018-05-14 15:56:21 +0800

    gameserver

--- server/Engine/Kernal/KernalLog.cpp
+++ server/Engine/Kernal/KernalLog.cpp
@@ -14,10 +14,11 @@ KernalLog::~KernalLog()
 
 void KernalLog::info( const char* pLogText, ... )
 {
-	va_list ap;
-    va_start( ap, pLogText );
-    char logbuff[1024] = { 0 };
-	sprintf( logbuff, pLogText, ap );
-    va_end( ap );
+	va_list args;
+    va_start( args, pLogText );
+    char logbuff[1024];
+	memset( logbuff, 0, sizeof(logbuff) );
+	vsprintf( logbuff, pLogText, args );
+    va_end( args );
 	printf("KernalLog::info: %s \n\r", logbuff);
 }
