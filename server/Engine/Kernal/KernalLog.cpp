15503a2c4feaefa1919362b8a21cdb305631801a
jiangxiangxuan <jiangxiangxuan@126.com>
2018-05-14 16:50:42 +0800

    gameserver

--- server/Engine/Kernal/KernalLog.cpp
+++ server/Engine/Kernal/KernalLog.cpp
@@ -2,9 +2,19 @@
 #include "KernalLog.h"
 #include <stdarg.h>
 
+#define LOG_BUFF_LINE_SIZE 1024  // 每一行长度
+
+void *KernalLogWorker( void *arg )
+{
+	KernalLog *pLog = (KernalLog*)arg;
+	pLog->logWorker();
+	return ((void *)0);
+}
+
 KernalLog::KernalLog()
 {
-	
+	memset( m_pLogFilePath, 0, sizeof( m_pLogFilePath ) );
+	memset( m_logPipe, 0, sizeof( m_logPipe ) );
 }
 
 KernalLog::~KernalLog()
@@ -12,13 +22,57 @@ KernalLog::~KernalLog()
 
 }
 
+void KernalLog::init( const char *pLogFilePath )
+{
+	memccpy( m_pLogFilePath, pLogFilePath, strlen( pLogFilePath ) );
+		
+    if( pipe( m_logPipe ) )
+    {
+        return false;
+    }
+	
+	m_LogThread.init(KernalServerBaseWorker, this);
+	m_LogThread.detach();
+		
+	return true;
+}
+
 void KernalLog::info( const char* pLogText, ... )
 {
 	va_list args;
     va_start( args, pLogText );
-    char logbuff[1024];
+    char logbuff[ LOG_BUFF_LINE_SIZE ];
 	memset( logbuff, 0, sizeof(logbuff) );
 	vsprintf( logbuff, pLogText, args );
     va_end( args );
 	printf("KernalLog::info: %s \n\r", logbuff);
+	
+	::write( m_logPipe[1], logbuff, strlen( logbuff ) );
+}
+
+void KernalLog::logWorker()
+{
+	m_logFd = open( m_pLogFilePath, O_RDWR | O_CREAT );
+	char logbuff[ LOG_BUFF_LINE_SIZE ];
+	int retval = 0;
+	int ret =  0;
+	while( !m_quit )
+	{
+		fd_set rset;
+		FD_ZERO( &rset );
+		FD_SET( m_logPipe[0], &rset );
+		retval = ::select( m_logPipe[0] + 1, &rset, NULL, NULL, NULL );
+		if( retval > 0 && FD_ISSET(pPipe->pipe[1], &rset)  )
+		{
+			memset( logbuff, 0, sizeof(logbuff) );
+			ret = ::read( m_logPipe[0], logbuff, sizeof(logbuff) );
+			if( ret > 0 )
+			{
+				::lseek( m_logFd, 0, SEEK_END );
+				::write( m_logFd, logbuff, ret );
+			}
+		}
+	}
+	
+	::close( m_logFd );
 }
--- server/Engine/Kernal/KernalLog.h
+++ server/Engine/Kernal/KernalLog.h
@@ -3,22 +3,34 @@
 #define _KERNAL_LOG_H_
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <errno.h>
 #include <malloc.h>
 #include <string.h>
 #include <string>
 
 #include "KernalLock.h"
+#include "KernalThread.h"
 
 class KernalLog
 {
 public:
     KernalLog();
     ~KernalLog();
+	
+	bool init( const char *pLogFilePath );
+	void logWorker();
 public:
-	void info( const char* pLogText, ... );
+	void info( const char *pLogText, ... );
 private:
+	KernalThread m_LogThread;
+	char 		 m_pLogFilePath[256];
+	int  		 m_logFd;
+	
+	int  		 m_logPipe[2];
+	bool 		 m_quit;
 };
 
 #endif
